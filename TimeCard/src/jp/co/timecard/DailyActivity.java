f4d243822177797e507e3d6694c3f9312ce873eb
flada-auxv <aseknock@gmail.com>
2012-08-05 04:20:15 +0900

    とりあえず日時データを入力・表示される所まで実装。DB登録および月次画面への反映は行っていない

--- TimeCard/src/jp/co/timecard/DailyActivity.java
+++ TimeCard/src/jp/co/timecard/DailyActivity.java
@@ -1,53 +1,86 @@
 package jp.co.timecard;
 
+import java.text.DecimalFormat;
 import java.util.Calendar;
 
 import android.app.Activity;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class DailyActivity extends Activity{
+public class DailyActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.daily_main);
+
+		Button bAttendance = (Button) findViewById(R.id.button_set_attendance);
+		bAttendance.setOnClickListener(new MyListener(R.id.daily_attendance));
 		
-		final Context context = this;
-		Button bAttendance = (Button)findViewById(R.id.button_set_attendance);
-		bAttendance.setOnClickListener(new View.OnClickListener() {
+		Button bLeave = (Button) findViewById(R.id.button_set_leave);
+		bLeave.setOnClickListener(new MyListener(R.id.daily_leave));
+		
+		Button bBreak = (Button) findViewById(R.id.button_set_break);
+		bBreak.setOnClickListener(new MyListener(R.id.daily_break));
+	}
+
+	private class MyListener implements OnClickListener {
+		int layout_id;
+		int hourOfDay;
+		int minute;
+		boolean is24HourView = true;
+		
+		public MyListener(int layout_id) {
+			super();
+			
+			switch (layout_id) {
+			case R.id.daily_attendance:
+			case R.id.daily_leave:
+				this.layout_id = layout_id;
+				break;
+			case R.id.daily_break:
+				this.layout_id = layout_id;
+				this.hourOfDay = 1;
+				break;
+			}
+		}
+		
+		// 各ボタンのリスナー 
+		public void onClick(View v) {
+			// attendance,leaveなら現在時刻とする breakなら初期化時の1を使用
+			if (hourOfDay != 1) {
+				Calendar calendar = Calendar.getInstance();
+				hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+				minute = calendar.get(Calendar.MINUTE);
+				is24HourView = true;
+			}
+			
+			new TimePickerDialog(DailyActivity.this, new TPDialogListener(layout_id), hourOfDay, minute, is24HourView).show();
+		}
+
+		private class TPDialogListener implements TimePickerDialog.OnTimeSetListener {
+			int layout_id;
+
+			public TPDialogListener(int layout_id) {
+				super();
+				this.layout_id = layout_id;
+			}
 
 			@Override
-			public void onClick(View v) {
-				Log.d("debug","onClick");
-				Calendar calendar = Calendar.getInstance();
-				int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
-				int minute = calendar.get(Calendar.MINUTE);
-				boolean is24HourView = false;
-				
-				new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
-					
-					@Override
-					public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-						// TODO DB登録
-						TextView tv = (TextView) findViewById(R.id.daily_attendance);
-						StringBuilder sb = new StringBuilder();
-						sb.append(Integer.toString(hourOfDay));
-						sb.append(":");
-						sb.append(Integer.toString(minute));
-						tv.setText(sb);
-					}
-				}, hourOfDay, minute, is24HourView).show();
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				// TODO DB登録、breakの時にもamとか表示されるのダサい...
+				TextView tv = (TextView) findViewById(layout_id);
+				DecimalFormat df = new DecimalFormat("00");
+				StringBuilder sb = new StringBuilder()
+				.append(df.format(hourOfDay))
+				.append(":")
+				.append(df.format(minute));
+				tv.setText(sb);
 			}
-		});
+		}
 	}
 }
-
-
-
-
-
